5f7d6837203bb50f93976887328c28772621f4d4
Zack Allen <zma4580@gmail.com>
2017-07-27 12:24:53 -0700

    Use package data in setup

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@
 from setuptools import setup
 
 setup(name='ftw',
-      version='1.1.3',
+      version='1.1.4',
       description='Framework for Testing WAFs',
       author='Chaim Sanders, Zack Allen',
       author_email='zma4580@gmail.com, chaim.sanders@gmail.com',
       url='https://www.github.com/crs-support/ftw',
-      download_url='https://github.com/crs-support/ftw/tarball/1.1.3',
+      download_url='https://github.com/crs-support/ftw/tarball/1.1.4',
       include_package_data=True,
+      package_data={
+        'ftw': ['util/public_suffix_list.dat']
+      },
       entry_points = {
         'pytest11': [
             'ftw = ftw.pytest_plugin'
