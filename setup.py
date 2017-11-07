763ab957f667bbebe3c85c0fbb8127429a65731b
Zack Allen <zma4580@gmail.com>
2017-11-07 13:35:28 -0500

    1.1.5 Setup.py bump

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name='ftw',
-      version='1.1.4',
+      version='1.1.5',
       description='Framework for Testing WAFs',
       author='Chaim Sanders, Zack Allen',
       author_email='zma4580@gmail.com, chaim.sanders@gmail.com',
       url='https://www.github.com/crs-support/ftw',
-      download_url='https://github.com/crs-support/ftw/tarball/1.1.4',
+      download_url='https://github.com/crs-support/ftw/tarball/1.1.5',
       include_package_data=True,
       package_data={
         'ftw': ['util/public_suffix_list.dat']
