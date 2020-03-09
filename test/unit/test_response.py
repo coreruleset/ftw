430a09c2ba181c7ba72c3687926e9d7bcd229281
Federico G. Schwindt <fgsch@lodoss.net>
2020-03-09 09:57:10 +0000

    Move this to unit test

---
+++ test/unit/test_response.py
@@ -0,0 +1,28 @@
+from ftw import testrunner, http, errors
+import pytest
+import re
+
+
+def test_response_before_response():
+    runner = testrunner.TestRunner()
+    http_ua = http.HttpUA()
+    with pytest.raises(errors.TestError):
+        runner.test_response(http_ua.response_object,
+                             re.compile('dog'))
+
+
+def test_response_failure():
+    runner = testrunner.TestRunner()
+    http_ua = http.HttpUA()
+    with pytest.raises(AssertionError):
+        runner.test_response(http.HttpResponse(
+                             "HTTP/1.1 200 OK\r\n\r\ncat", http_ua),
+                             re.compile('dog'))
+
+
+def test_response_success():
+    runner = testrunner.TestRunner()
+    http_ua = http.HttpUA()
+    runner.test_response(http.HttpResponse(
+                         "HTTP/1.1 200 OK\r\n\r\ncat", http_ua),
+                         re.compile('cat'))
