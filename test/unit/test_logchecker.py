97c88e150c02a5da4a6fe821015a9fa0eae2441b
Federico G. Schwindt <fgsch@lodoss.net>
2020-03-09 10:05:21 +0000

    More tests

--- test/unit/test_logchecker.py
+++ test/unit/test_logchecker.py
@@ -2,6 +2,15 @@ from ftw import logchecker
 import pytest
 
 
-def test_logchecker():
+def test_logchecker_abstract():
     with pytest.raises(TypeError):
         logchecker.LogChecker()
+
+
+class LogChecker(logchecker.LogChecker):
+    def get_logs(self):
+        return None
+
+
+def test_logchecker_impl():
+    LogChecker()
